798c0c5f2b52f8f98dbec1ba89e09bfa2c724d0f
zrferris-glitch <zrferris@gmail.com>
2025-12-11 23:04:08 -0700

    Win Condition and Alternative Turn System
    Added win condition, points for each player depending on their order of winning, and a simple, albeit janky, turn system tweaked to work with the win condition

--- GIMM 110 AR Game/Assets/Scripts/PlayerInfo.cs
+++ GIMM 110 AR Game/Assets/Scripts/PlayerInfo.cs
@@ -16,14 +16,18 @@ public class PlayerInfo : MonoBehaviour
                                   * This variable will be sent as a parameter to the timermanager script and then sent to the coroutine. 
                                   * It will be sent in the Enter method in the minigame state class for whatever minigame.
                                   */
-    public float coinMultiplier; /*This is by how much the coin multiplier will be.
+    public float coinAdditive = 2; /*This is by how much the coin multiplier will be.
                                   * You pick up 1 coin for 1 point, but then it is * by this variable. 
                                   * I am thinking 1.5x, but I think we would have to round the float to an int since the scoreTotal is an int
-                                  */
+                                  SUBJECT TO CHANGE*/
 
-    public float playerNumber;    /*This is referring to the player order for turns. Each turn, it'll change each player's number to cycle the order. 
-                                  Not needed?
-                                  */
+    public float playerEndPlacement;    /*This is for tracking the player's ranking (1st, 2nd, 3rd) in the game. Only counted at the end of the game
+                                        */
+
+    public float firstPlacePrize = 25;
+    public float secondPlacePrize = 20;
+    public float thirdPlacePrize = 15;
+    public float consolationPrize = 10; //4th place
 
     #endregion
     // Start is called once before the first execution of Update after the MonoBehaviour is created
